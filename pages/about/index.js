32527800ad861d9617e159a7c3e67b0c1c345e62
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-11 14:06:32 +0600

    about page static data added

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import styles from '../../styles/Home.module.css';
 
 function AboutUs() {
   return (
-    <div>
+    <div className={styles.main}>
       <h1>This is our About Us page</h1>
+      <p>
+        Sent oer my the from my tinkled. Have and shore the was from than
+        mefilled nothing. Some angels being living some nevernevermore for the
+        remember dreary. Tis dream leave be ah, ebony thrilled some a and yet
+        beating. His at the my denser whether and censer, in ever home thy
+        silken borrow, felt tempest that fowl uttered nevermore doubting nothing
+        floor doubtless, one obeisance surely entrance surely beguiling of of,
+        of what more door a raven sitting. Of name broken perched raven
+        sorrowsorrow my whether. And lamplight nothing of beguiling my the
+        longer, the sorrow of late soon engaged beguiling.{' '}
+      </p>
     </div>
   );
 }
