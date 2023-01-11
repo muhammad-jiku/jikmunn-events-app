9341babbef9f6c974fd2e48f0c94848f2c284db2
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-11 14:05:05 +0600

    static data added to events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import styles from '../../styles/Home.module.css';
 
 function Events() {
   return (
-    <div>
+    <div className={styles.main}>
       <h1>This is our events page</h1>
+      <div>
+        <a href="">
+          <img src="" alt="" />
+          <h2>Events in London</h2>
+        </a>
+
+        <a href="">
+          <img src="" alt="" />
+          <h2>Events in Doha</h2>
+        </a>
+
+        <a href="">
+          <img src="" alt="" />
+          <h2>Events in Dhaka</h2>
+        </a>
+      </div>
     </div>
   );
 }
