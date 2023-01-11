118ce4c6aae24728d25e4c9a6d0cd039943f6231
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-11 13:21:21 +0600

    events page added

---
+++ pages/events/event1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function event1() {
+  return (
+    <div>
+      <h1>event1</h1>
+    </div>
+  );
+}
+
+export default event1;
---
+++ pages/events/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Events() {
+  return (
+    <div>
+      <h1>This is our events page</h1>
+    </div>
+  );
+}
+
+export default Events;
