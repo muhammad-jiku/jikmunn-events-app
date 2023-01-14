1d26188044dc6e42e8adfc86886742aca94fba2d
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-14 21:39:46 +0600

    necssary changes in home component

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -7,23 +7,23 @@ const HomePage = ({ data }) => {
   return (
     <div className="home_body">
       {data.map((event, idx) => (
-        <Link key={idx} href={`/events/${event.id}`} passHref>
-          {/* <Link key={idx} href={`/events/${event.id}`} passHref legacyBehavior>
-          <a className="card" href={`/events/${event.id}`}> */}
-          <Link className="card" href={`/events/${event.id}`}>
+        <Link key={idx} href={`/events/${event.id}`} passHref legacyBehavior>
+          <a className="card" href={`/events/${event.id}`}>
+            {/* <Link className="card" href={`/events/${event.id}`}> */}
             <div className="image">
               <Image
                 src={`${event.image}`}
                 alt={`${event.title}`}
-                width={500}
-                height={250}
+                width={480}
+                height={380}
               />
             </div>
             <div className="content">
               <h2>{event.title}</h2>
               <p>{event.description}</p>
             </div>
-          </Link>
+          </a>
+          {/* </Link> */}
         </Link>
       ))}
     </div>
