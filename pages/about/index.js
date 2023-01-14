6226c8ee681b74f735ac645a7810c70c2be2c9b9
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-14 20:45:59 +0600

    about us page modified

--- pages/about/index.js
+++ pages/about/index.js
@@ -14,8 +14,40 @@ function AboutUs() {
         floor doubtless, one obeisance surely entrance surely beguiling of of,
         of what more door a raven sitting. Of name broken perched raven
         sorrowsorrow my whether. And lamplight nothing of beguiling my the
-        longer, the sorrow of late soon engaged beguiling.{' '}
+        longer, the sorrow of late soon engaged beguiling.
       </p>
+      <h2>Dlor in reprehenderit </h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <ul className="about_us_list">
+        <li>
+          <h3>Dlor in reprehenderit </h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in
+          </p>
+        </li>
+        <li>
+          <h3>Sed do eiusmod tempor </h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in
+          </p>
+        </li>
+      </ul>
     </div>
   );
 }
