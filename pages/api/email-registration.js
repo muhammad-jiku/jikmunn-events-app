f87467f533792c45ebc4587c33a31675cd31628d
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-15 13:36:14 +0600

    important changes in api file

--- pages/api/email-registration.js
+++ pages/api/email-registration.js
@@ -1,8 +1,54 @@
+import path from 'path';
+import fs from 'fs';
+
+function buildPath() {
+  return path.join(process.cwd(), 'data', 'data.json');
+}
+
+function extractData(filePath) {
+  const jsonData = fs.readFileSync(filePath);
+  const data = JSON.parse(jsonData);
+  return data;
+}
+
 export default function handler(req, res) {
   const { method } = req;
 
+  const filePath = buildPath();
+  const data = extractData(filePath);
+  const { events_categories, allEvents } = data;
+
+  if (!allEvents) {
+    return res.status(404).json({
+      status: 404,
+      message: 'Events data is not found',
+    });
+  }
+
   if (method === 'POST') {
     const { email, eventId } = req.body;
+
+    const newAllEvents = allEvents.map((event) => {
+      if (event.id === eventId) {
+        if (event.emails_registered.includes(email)) {
+          res
+            .status(409)
+            .json({ message: 'This email has already been registered' });
+          return event;
+        }
+        return {
+          ...event,
+          emails_registered: [...event.emails_registered, email],
+        };
+      }
+      return event;
+    });
+
+    fs.writeFileSync(
+      filePath,
+      JSON.stringify({ events_categories, allEvents: newAllEvents })
+    );
+
     res.status(200).json({
       message: `${email} has been registered a ticket successfully for ${eventId}! event.`,
       email,
