3e87c0ecb968be54b39a83cc06d56b6f4384239f
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-15 11:53:43 +0600

    form added on single event component

--- src/components/Events/SingleEvent.jsx
+++ src/components/Events/SingleEvent.jsx
@@ -7,6 +7,17 @@ const SingleEventPerId = ({ data }) => {
       <h1> {data.title} </h1>
       <Image src={data.image} width={1000} height={700} alt={data.title} />
       <p> {data.description} </p>
+
+      <form onSubmit={onSubmit} className="email_registration">
+        <label> Get Registered for this event!</label>
+        <input
+          ref={inputEmail}
+          type="email"
+          id="email"
+          placeholder="Please insert your email here"
+        />
+        <button type="submit"> Submit</button>
+      </form>
     </div>
   );
 };
